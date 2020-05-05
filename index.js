47ed06265b74871b3b5ceb0ca2d8149dbd22fd3c
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-05 13:16:39 -0700

    server and router setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = require('./router')
+
+const server = express()
+server.use(express.json())
+server.use('./api/posts', router)
+
+
+server.listen(9000, () => console.log('The API works'))
