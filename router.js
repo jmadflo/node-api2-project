d89717026a2b830e81839ff53fc32676b2459944
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-05 16:01:06 -0700

    can now add comments

--- router.js
+++ router.js
@@ -6,8 +6,8 @@ const router = express.Router()
 // base url for these routes is http://localhost:9000/api/posts
 
 router.post('/', (req, res) => {
+    // res.send(req.body)
     // if title or contents are missing, give error 400
-    // res.json(req.body)
     if (req.body.title && req.body.contents){
         // if insertion is successful return status 201 otherwise return status 500 error
         data.insert(req.body)
@@ -26,6 +26,35 @@ router.post('/', (req, res) => {
             })
     } else {
         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+})
+
+router.post('/:id/comments', (req, res) => {
+    // res.send(req.body)
+    // return 404 error if comment does not have a text value
+    if (req.body.text) {
+        // find the post that the comment is supposed to be for and return 404 error if it isn't found
+        data.findById(req.params.id)
+            .then(targetPost => {
+                // insert the comment to the post and return error 500 if not successful
+                data.insertComment({ text: req.body.text, post_id: targetPost[0].id })
+                    .then(commentIdObject => {
+                        // retrieve new comment and return it to the client
+                        data.findCommentById(commentIdObject.id)
+                            .then(insertedComment => {
+                                res.status(201).json(insertedComment)
+                            })
+                            .catch(error => alert(error))
+                    })
+                    .catch(() => {
+                        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                    })
+            })
+            .catch(() => {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
     }
 })
 
