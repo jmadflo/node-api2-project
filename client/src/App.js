0d9121b507d91b5ccc172c63ed005dee63bbfd71
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-06 01:03:43 -0700

    react app styled and complete

--- client/src/App.js
+++ client/src/App.js
@@ -69,76 +69,87 @@ function App() {
   }
 
   // post a new user
-  // const postNew = () => {
-  //   axios.create({headers: {'Content-Type': 'application/json'}})
-  //     .post('http://localhost:8000/api/dataToRender/', {
-  //       name: formValues.name,
-  //       bio: formValues.bio
-  //     })
-  //     .then(response => {
-  //       console.log(response)
-  //     })
-  //     .catch(error => console.log(error))
-  // }
+  const postNewPost = () => {
+    axios.create({headers: {'Content-Type': 'application/json'}})
+      .post('http://localhost:9000/api/posts/', {
+        title: postFormValues.title,
+        contents: postFormValues.contents
+      })
+      .then(response => {
+        console.log(response)
+        setDataToRender(response.data)
+      })
+      .catch(error => console.log(error))
+  }
 
-  
+  // post a new comment by post id
+  const postNewComment = () => {
+    axios.create({headers: {'Content-Type': 'application/json'}})
+      .post(`http://localhost:9000/api/posts/${postFormValues.id || commentFormValues.id}/comments`, {
+        text: commentFormValues.text
+      })
+      .then(response => {
+        console.log(response)
+        setDataToRender(response.data)
+      })
+      .catch(error => console.log(error))
+  }
 
-  // // delete user by id
-  // const deleteUser = () => {
-  //   axios.delete(`http://localhost:8000/api/dataToRender/${formValues.id}`)
-  //     .then(response => {
-  //       console.log(response)
-  //     })
-  //     .catch(error => console.log(error))
-  // }
+  // delete post by id
+  const deletePost = () => {
+    axios.delete(`http://localhost:9000/api/posts/${postFormValues.id || commentFormValues.id}`)
+      .then(response => {
+        console.log(response)
+        setDataToRender([response.data])
+      })
+      .catch(error => console.log(error))
+  }
 
-  // // edit user with put request
-  // const editUser = () => {
-  //   axios.create({headers: {'Content-Type': 'application/json'}})
-  //     .put(`http://localhost:8000/api/dataToRender/${formValues.id}`, {
-  //       name: formValues.name,
-  //       bio: formValues.bio
-  //     })
-  //     .then(response => {
-  //       console.log(response)
-  //     })
-  //     .catch(error => console.log(error))
-  // }
+  // edit post with post id
+  const editPost = () => {
+    axios.create({headers: {'Content-Type': 'application/json'}})
+      .put(`http://localhost:9000/api/posts/${postFormValues.id || commentFormValues.id}`, {
+        title: postFormValues.title,
+        contents: postFormValues.contents
+      })
+      .then(response => {
+        console.log(response)
+        setDataToRender(response.data)
+      })
+      .catch(error => console.log(error))
+  }
 
 
   return (
     <div className='App'>
       <h1>API Tester</h1>
       <div className='formAndButtons'>
-        <form>
-          <h2>Post Form</h2>
-          <input name='id' placeholder='id' value={postFormValues.id} onChange={updatePostForm}/>
-          <input name='title' placeholder='title' value={postFormValues.title} onChange={updatePostForm}/>
-          <input name='contents' placeholder='contents' value={postFormValues.contents} onChange={updatePostForm}/>
-          <input name='created_at' placeholder='created_at' value={postFormValues.created_at} onChange={updatePostForm}/>
-          <input name='updated_at' placeholder='updated_at' value={postFormValues.updated_at} onChange={updatePostForm}/>
-        </form>
-        <form>
-          <h2>Comment Form</h2>
-          <input name='id' placeholder='id' value={commentFormValues.id} onChange={updateCommentForm}/>
-          <input name='text' placeholder='text' value={commentFormValues.text} onChange={updateCommentForm}/>
-          <input name='created_at' placeholder='created_at' value={commentFormValues.created_at} onChange={updateCommentForm}/>
-          <input name='updated_at' placeholder='updated_at' value={commentFormValues.updated_at} onChange={updateCommentForm}/>
-          <input name='post_id' placeholder='post_id' value={commentFormValues.post_id} onChange={updateCommentForm}/>
-          <input name='post' placeholder='post' value={commentFormValues.post} onChange={updateCommentForm}/>
-        </form>
+        <div className='formContainer'>
+          <form>
+            <h2>Post Form</h2>
+            <input name='id' placeholder='id' value={postFormValues.id} onChange={updatePostForm}/>
+            <input name='title' placeholder='title' value={postFormValues.title} onChange={updatePostForm}/>
+            <input name='contents' placeholder='contents' value={postFormValues.contents} onChange={updatePostForm}/>
+          </form>
+          <form>
+            <h2>Comment Form</h2>
+            <input name='id' placeholder='post id' value={commentFormValues.id} onChange={updateCommentForm}/>
+            <input name='text' placeholder='text' value={commentFormValues.text} onChange={updateCommentForm}/>
+          </form>
+        </div>
         <div className='buttonContainer'>
           <button onClick={getAllPosts}>Get All Posts</button>
           <button onClick={getPostById}>Get Post By Id</button>
           <button onClick={getCommentsById}>Get Comments By Post Id</button>
-          {/* <button onClick={postNewPost}>Post New Post</button>
+          <button onClick={postNewPost}>Post New Post</button>
           <button onClick={postNewComment}>Post New Comment</button>
           <button onClick={deletePost}>Delete Post</button>
-          <button onClick={editPost}>Edit Post</button> */}
+          <button onClick={editPost}>Edit Post</button>
         </div>
       </div>
       {/* render data */}
       {dataToRender.map(instance => {
+        // will render if instance is a post
         if (instance.title){
           return (
             <div key={instance.id} className='instance'>
@@ -149,7 +160,13 @@ function App() {
               <p>Updated At: {instance.updated_at}</p>
             </div>
           )
-        } else {
+        } else if (instance.message){ // will render if instance is a message object
+          return (
+            <div key={instance.message} className='instance'>
+              <p>Message: {instance.message}</p>
+            </div>
+          )
+        } else { // will render if instance is a comment
           return (
             <div key={instance.id} className='instance'>
               <p>Id: {instance.id}</p>
@@ -166,24 +183,4 @@ function App() {
   )
 }
 
-// [
-//   {
-//       "id": 1,
-//       "title": "Thoughts of mine",
-//       "contents": "cool and interesting stuff as well located here!!!",
-//       "created_at": "2019-05-11 01:55:52",
-//       "updated_at": "2019-05-11 01:55:52"
-//   }
-// ]
-
-// [
-//   {
-//       "id": 7,
-//       "text": "A good travel has no fixed plans and is not intent upon arriving. A good artist lets his intuition lead him where ever it wants.",
-//       "created_at": "2019-05-11 01:55:52",
-//       "updated_at": "2019-05-11 01:55:52",
-//       "post_id": 5,
-//       "post": "You need people of intelligence on this sort of mission...quest...thing."
-//   }
-// ]
 export default App
